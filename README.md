2fcc600177ef8cf863ce7bdc672d098f3bc58039
cbaziotis <christos.baziotis@gmail.com>
2017-02-11 22:27:59 +0200

    doc

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-Text processing tool
+Text processing tool, offering tokenization targeting text from social networks (twitter, facebook...), spell correction and word segmentation using word statistics from 2 different corpora (english Wikipedia, twitter - 330mil tweets)
+###Tokenization
+The tokenizer understands many expressions found in natural language such as dates, times, currencies, hashtags, emoticons and much more...
+###Spell Correction
+The Spell Corrector extends the functionality of Peter Norvig's spell-corrector.
+###Word Segmentation
+Word Segmentation that implements the Viterbi algorithm for word segmentation. Based on CH14 from the book Beautiful Data (Segaran and Hammerbacher, 2009)
+
+
+documentation and examples coming soon...
--- example.py
+++ example.py
@@ -17,7 +17,19 @@ text_processor = TextPreProcessor(
 sentences = [
     "CANT WAIT for the new season of #TwinPeaks ＼(^o^)／ !!! #DavidLynch #tvseries :) ",
     "The new #johndoe suuuuucks!!! WAISTED $10... #badmovies >:/",
+    "Sophos aims to raise $100m in London IPO",
+    "When there are no words , use emojis 😎",
+    "The sound of #Sunday with Bee Gees - Alive https://t.co/CvHFOMO1tN #webradio #internetradio",
+    "I hope Paul Dunne blows up tomorrow only because UAB beats us in the tourney... #stillnotoverit",
+    "It's National Ice Cream Day on Sunday, July 19. Get the ice cream scoop.  http://t.co/S6yXXQHzHk #nationalicecreamday",
+    "Watchman may have drowned on the job  On 16.07.15, at 9:11 p.m., police responded to a report of a watchman found... http://t.co/UdEXjQFeqq",
+    "The Week in Ransomware - December 16th 2016 - Samas, No More Ransom, Screen Lockers, and More https://t.co/MWgeYhnMk7 #technology https://t.co/Wzb1qoU6cG",
+    "#FantasticBeasts is an excellent movie, in my opinion, but I still can't wait for @cinemasins to sin the sh*t out of it",
+    "Not sure how I feel about the Black Keys playing at the U-Village Microsoft store. Guess they *are* getting paid, that's good. #Microsoft",
+    "Good night #Twitter and #TheLegionoftheFallen.  5:45am cimes awfully early!",
 ]
 
 for s in sentences:
+    print()
+    print(s)
     print(" ".join(text_processor.pre_process_doc(s)))
