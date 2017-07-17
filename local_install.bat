59a2c5fff1a351bf3c0881e145e56da58311d416
Christos Baziotis <christos.baziotis@gmail.com>
2017-07-17 11:55:05 +0300

    git fix

--- local_install.bat
+++
@@ -1,7 +0,0 @@
-RMDIR /S /Q build
-RMDIR /S /Q keras_utilities.egg-info
-RMDIR /S /Q dist
-
-python setup.py sdist bdist_wheel
-
-pip install --no-index --find-links=dist\ ekphrasis --force-reinstall --no-deps -U
