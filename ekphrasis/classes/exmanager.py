f1e698cd99501b4509bda5ffe65ea2a0e41ea4be
Christos Baziotis <christos.baziotis@gmail.com>
2017-10-06 20:46:18 +0300

    fix static data files

--- ekphrasis/classes/exmanager.py
+++ ekphrasis/classes/exmanager.py
@@ -1,15 +1,20 @@
 import json
+import os
 import re
 
 
 class ExManager:
+    ext_path = os.path.join(os.path.dirname(__file__),
+                            '../regexes/expressions.txt')
 
-    with open('../regexes/expressions.txt') as fh:
+    with open(ext_path) as fh:
         expressions = json.load(fh)
 
     def get_compiled(self):
-        regexes = {k.lower(): re.compile(self.expressions[k]) for k, v in self.expressions.items()}
+        regexes = {k.lower(): re.compile(self.expressions[k]) for k, v in
+                   self.expressions.items()}
         return regexes
 
     def print_expressions(self):
-        {print(k.lower(), ":", self.expressions[k]) for k, v in sorted(self.expressions.items())}
+        {print(k.lower(), ":", self.expressions[k])
+         for k, v in sorted(self.expressions.items())}
--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(name='ekphrasis',
-      version='0.2.0',
-      description='Text processing tool, geared towards text from social networks, such as Twitter or Facebook. Ekphrasis performs tokenization, word normalization, word segmentation (for splitting hashtags) and spell correction.',
+      version='0.3.5',
+      description='Text processing tool, geared towards text from '
+                  'social networks, such as Twitter or Facebook. '
+                  'Ekphrasis performs tokenization, word normalization, '
+                  'word segmentation (for splitting hashtags) '
+                  'and spell correction.',
       url='https://github.com/cbaziotis/ekphrasis',
       author='Christos Baziotis',
       author_email='christos.baziotis@gmail.com',
